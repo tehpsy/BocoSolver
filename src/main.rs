23a0d9c586a76d2a36375f9cd8112f6c72223968
James Baxter <james.baxter@focusrite.com>
2020-12-21 10:20:41 +0800

    Move builder function to separate module

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,8 @@ use model::*;
 
 mod utils;
 
+mod builder;
+
 //look for hashing a hashmap in rust
 
 fn calculate_hash<T: Hash>(t: &T) -> u64 {
@@ -24,83 +26,7 @@ fn main() {
     let rc = RefCell::new(graph); 
     let c = Rc::new(rc);
 
-    let first_board = Board{
-        player: Player{block_id: 8},
-        blocks: hashmap!{
-            0 => Block{
-                small: None,
-                large: None,
-                id: 0,
-                neighbour_ids: NeighbourIds::new(None, Some(5), None, Some(1))
-            },
-            1 => Block{
-                small: None,
-                large: None,
-                id: 1,
-                neighbour_ids: NeighbourIds::new(None, Some(6), Some(0), Some(2))
-            },
-            2 => Block{
-                small: None,
-                large: None,
-                id: 2,
-                neighbour_ids: NeighbourIds::new(None, Some(7), Some(1), Some(3))
-            },
-            3 => Block{
-                small: None,
-                large: Some(Unit{
-                    orientation: Orientation::Down,
-                    color: Color::Black,
-                }),
-                id: 3,
-                neighbour_ids: NeighbourIds::new(None, Some(8), Some(2), Some(4))
-            },
-            4 => Block{
-                small: None,
-                large: None,
-                id: 4,
-                neighbour_ids: NeighbourIds::new(None, Some(9), Some(3), None)
-            },
-            5 => Block{
-                small: None,
-                large: None,
-                id: 5,
-                neighbour_ids: NeighbourIds::new(Some(0), None, None, Some(6))
-            },
-            6 => Block{
-                small: None,
-                large: Some(Unit{
-                    orientation: Orientation::Up,
-                    color: Color::Red,
-                }),
-                id: 6,
-                neighbour_ids: NeighbourIds::new(Some(1), None, Some(5), Some(7))
-            },
-            7 => Block{
-                small: None,
-                large: Some(Unit{
-                    orientation: Orientation::Left,
-                    color: Color::Black,
-                }),
-                id: 7,
-                neighbour_ids: NeighbourIds::new(Some(2), None, Some(6), Some(8))
-            },
-            8 => Block{
-                small: None,
-                large: None,
-                id: 8,
-                neighbour_ids: NeighbourIds::new(Some(3), None, Some(7), Some(9))
-            },
-            9 => Block{
-                small: Some(Unit{
-                    orientation: Orientation::Up,
-                    color: Color::Red,
-                }),
-                large: None,
-                id: 9,
-                neighbour_ids: NeighbourIds::new(Some(4), None, Some(8), None)
-            },
-        }
-    };
+    let first_board = builder::build_hardest_boco_level();
     let first_board_hash = calculate_hash(&first_board);
 
     let mut boards: HashMap<u64, Board> = hashmap!{};
