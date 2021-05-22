f0372ce755846797159210e97f79631a7ef707f8
James Baxter <james.baxter@focusrite.com>
2021-05-23 07:59:42 +0800

    Add more info about default boards

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,13 @@ use std::time::{Duration, Instant};
 
 fn main() {
     let start1 = Instant::now();
-    // let boards = builder::build(3, 3, 0, 0, 1, 1);
-    // let boards = builder::build(1, 3, 0, 0, 1, 1);
-    // let boards = builder::build(2, 5, 1, 1, 1, 1);
-    // let boards = builder::build(3, 3, 1, 1, 1, 1);
-    // let boards = builder::build(2, 4, 0, 1, 1, 1); // 107520 - 138s - max 29 moves - 0.0013s per board
-    // let boards = builder::build(2, 4, 0, 0, 1, 1); // 5376 - 10.4s - max 25 moves - 0.0019s per board
-    let boards = builder::build(2, 3, 1, 0, 1, 1); // 23040 - 5.3s - max 13 moves - 0.0002s per board
+    // let boards = builder::build(3, 3, 0, 0, 1, 1); // 3388 - 1.9s - 24
+    // let boards = builder::build(1, 3, 0, 0, 1, 1); // 6 - tiny - 2
+    // let boards = builder::build(2, 5, 1, 1, 1, 1); // 1010160 - ?s - 59
+    // let boards = builder::build(3, 3, 1, 1, 1, 1); // 529080 - 374s - 52
+    let boards = builder::build(2, 4, 0, 1, 1, 1); // 21360 - 4.3s - 29
+    // let boards = builder::build(2, 4, 0, 0, 1, 1); // 1968 - 0.5s - 25
+    // let boards = builder::build(2, 3, 1, 0, 1, 1); // 3312 - 0.1s - 13
     println!("Creating {} boards took {:?} seconds", boards.len(), start1.elapsed());
 
     // let n = 10000;
@@ -28,7 +28,7 @@ fn main() {
         match solver::get_simplest_solution(&board) {
             Some((cost, path)) => {
                 // println!("Cost: {}", cost);
-                if cost >= 13 {
+                if cost >= 24 {
                     println!("Cost: {}", cost);
                     utils::print(&board);
                     println!("Route: {:?}", path);
@@ -39,6 +39,7 @@ fn main() {
     }
 
     println!("Analysing boards took {:?} seconds", start2.elapsed());
+    println!("Total: {:?} seconds", start1.elapsed());
 }
 
 #[cfg(test)]
