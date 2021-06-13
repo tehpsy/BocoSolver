64403a9d2588f6164ea2374796dd60c0209ff6f1
James Baxter <james.baxter@focusrite.com>
2021-06-13 15:15:32 +0800

    Cleanup

--- src/main.rs
+++ src/main.rs
@@ -17,8 +17,8 @@ fn main() {
     // let boards = builder::build(3, 3, 0, 0, 1, 1); // 3388 - 1.9s - 24
     // let boards = builder::build(1, 3, 0, 0, 1, 1); // 6 - tiny - 2
     // let boards = builder::build(2, 5, 1, 1, 1, 1); // 1010160 - ?s - 59
-    // let boards = builder::build(3, 3, 1, 1, 1, 1); // 529080(198405 condensed) - 374s - 53
-    let boards = builder::build(2, 4, 0, 1, 1, 1); // 21360 - 4.3s - 29
+    let boards = builder::build(3, 3, 1, 1, 1, 1); // 529080(198405 condensed) - 374s - 53
+    // let boards = builder::build(2, 4, 0, 1, 1, 1); // 21360 - 4.3s - 29
     // let boards = builder::build(2, 5, 2, 1, 1, 1);
     // let boards = builder::build(2, 4, 0, 0, 1, 1); // 1968 - 0.5s - 25
     // let boards = builder::build(2, 3, 1, 0, 1, 1); // 3312 - 0.1s - 13
@@ -27,22 +27,23 @@ fn main() {
     let start2 = Instant::now();
 
     let solutions: Vec<Solution> = boards
-        // .iter()
         .par_iter()
         .map(|board| { solver::get_simplest_solution(&board) })
         .filter_map(|x| x)
         .collect();
 
-    if let Some(max) = solutions.par_iter().max_by_key(|result| result.cost) {
-        let board = max.boards.get(&max.start_node.hash_id).unwrap();
-        println!("Cost: {}", max.cost);
-        utils::print(&board);
+    if let Some(max) = solutions
+        .par_iter()
+        .max_by_key(|result| result.cost) {
+            let board = max.boards.get(&max.start_node.hash_id).unwrap();
+            println!("Cost: {}", max.cost);
+            utils::print(&board);
 
-        if let Some(moves) = solver::moves_for(&max.boards, &max.nodes) {
-            println!("{:?}", moves);
-        } else {
-            println!("no moves found");
-        }
+            if let Some(moves) = solver::moves_for(&max.boards, &max.nodes) {
+                println!("{:?}", moves);
+            } else {
+                println!("no moves found");
+            }
     }
 
     println!("Analysing boards took {:?}", start2.elapsed());
